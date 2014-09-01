0710dde63ac0194826b3e057ce80b53a5347a281
Matt Kaniaris <mkaniaris@gmail.com>
2014-08-31 18:20:42 -0700

    add a few broken tests

--- tests/models.js
+++ tests/models.js
@@ -1,15 +1,52 @@
+"use strict"
+
+var flux = require("../lib/flux"),
+  FieldTypes = flux.FieldTypes;
+
 var tests = {
-  setUp: function (callback) {
-    this.foo = 'bar';
-    callback();
-  },
-  tearDown: function (callback) {
-    // clean up
-    callback();
-  },
-  test1: function (test) {
-    test.equals(this.foo, 'bar');
+  test_createModel: function(test) {
+    test.throws(function() {
+      flux.createModel({});
+    }, Error, "Empty models are not allowed");
     test.done();
+  },
+  test_cleanModelCreation: function(test) {
+    var Model = flux.createModel({
+      submodel: {
+        field: FieldTypes.string
+      }
+    });
+    tests.ok(new Model.submodel() instanceof Model.submodel);
+    test.done();
+  },
+  test_cleanCollectionCreation: function(test) {
+    var Model = flux.createModel({
+      submodel: [{
+        field: FieldTypes.string
+      }]
+    });
+    tests.ok(new Model.submodel() instanceof Model.submodel);
+    test.done();
+  },
+  test_cleanModelInstantiation: function (test) {
+    var Model, SubModel, SubModels;
+    Model = flux.createModel({
+      fieldTypes: {
+        submodels: SubModels 
+      }
+    });
+    SubModel = flux.createModel({
+      fieldTypes: {
+        field: FieldTypes.string
+      }
+    });
+    SubModels = flux.createCollection({
+      modelName: "SubModels",
+      model: SubModel
+    });
+    tests.ok(new Model({submodels: [{field: "asdf"}, {field: "qwer"}]}));
+    tests.done();
   }
 }
+
 module.exports = tests;
