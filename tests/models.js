a75596bc354e7923155510c27e1b3992ef01a1ff
Matt Kaniaris <mkaniaris@gmail.com>
2014-09-05 00:22:28 -0700

    fixed field name propogation and added tests

--- tests/models.js
+++ tests/models.js
@@ -2,33 +2,108 @@
 
 var flux = require("../lib/flux"),
   FieldTypes = flux.FieldTypes,
-  creation, instantiation;
+  creation, instantiation, events;
+
+events = {
+  dataModelFields1: function(test) {
+    var model, Model = flux.createModel({
+      fieldTypes: {
+        field: FieldTypes.string,
+      }
+    });
+    model = new Model();
+    model.on("field", function() {
+      test.done();
+    });
+    model.field = "asdf";
+  },
+  dataModelFields2: function(test) {
+    var model, Model = flux.createModel({
+      fieldTypes: {
+        field: FieldTypes.string,
+      }
+    });
+    model = new Model();
+    model.on(function(field) {
+      test.equals(field, "field");
+      test.done();
+    });
+    model.field = "asdf";
+  },
+  dataModelSubFields: function(test) {
+    var model, Model, SubModel = flux.createModel({
+      fieldTypes: {
+        subField: FieldTypes.string
+      }
+    });
+    Model = flux.createModel({
+      fieldTypes: {
+        field: SubModel,
+      }
+    });
+    model = new Model({field: new SubModel()});
+    model.on(function(field) {
+      test.equals(field, "field.subField");
+      test.done();
+    });
+    model.field.subField = "asdf";
+  },
+  dataCollectionSubFields: function(test) {
+    var model, Model, SubModel, SubModels,
+      testField = "asdf";
+
+    SubModel = flux.createModel({
+      modelName: "sub",
+      fieldTypes: {
+        field: FieldTypes.string
+      }
+    });
+    SubModels = flux.createCollection({
+      modelName: "subs",
+      model: SubModel
+    });
+    Model = flux.createModel({
+      modelName: "top",
+      fieldTypes: {
+        submodels: SubModels 
+      }
+    });
+    model = new Model();
+    model.submodels = new SubModels();
+    model.submodels.add(new SubModel({field: "asdf"}));
+    model.on(function(name) {
+      test.equals("submodels.field", name);
+      test.done();
+    });
+    model.submodels.get(0).field = "a";
+  },
+};
 
 creation = {
-  // test_createModel: function(test) {
-  //   test.throws(function() {
-  //     flux.createModel({});
-  //   }, Error, "Empty models are not allowed");
-  //   test.done();
-  // },
-  // test_conciseModelCreation: function(test) {
-  //   var Model = flux.createModel({
-  //     submodel: {
-  //       field: FieldTypes.string
-  //     }
-  //   });
-  //   test.ok(new Model.submodel() instanceof Model.submodel);
-  //   test.done();
-  // },
-  // subField: function(test) {
-  //   var Model = flux.createModel({
-  //     submodel: [{
-  //       field: FieldTypes.string
-  //     }]
-  //   });
-  //   test.ok(new Model.submodel() instanceof Model.submodel);
-  //   test.done();
-  // },
+  test_createModel: function(test) {
+    test.throws(function() {
+      flux.createModel({});
+    }, Error, "Empty models are not allowed");
+    test.done();
+  },
+  test_conciseModelCreation: function(test) {
+    var Model = flux.createModel({
+      submodel: {
+        field: FieldTypes.string
+      }
+    });
+    test.ok(new Model.submodel() instanceof Model.submodel);
+    test.done();
+  },
+  subField: function(test) {
+    var Model = flux.createModel({
+      submodel: [{
+        field: FieldTypes.string
+      }]
+    });
+    test.ok(new Model.submodel() instanceof Model.submodel);
+    test.done();
+  },
 };
 
 instantiation = {
@@ -78,5 +153,6 @@ instantiation = {
 
 module.exports = {
   creation: creation,
-  instantiation: instantiation
+  instantiation: instantiation,
+  events: events
 };
