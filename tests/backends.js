229183addb65ed89c713caaa57f02b3ae0c80923
Matt Kaniaris <mkaniaris@gmail.com>
2014-09-15 21:57:36 -0700

    added local storage backing for DataModels

---
+++ tests/backends.js
@@ -0,0 +1,36 @@
+var flux = require("../lib/flux"), 
+  _ = require("lodash"),
+  utils = require("../lib/utils"),
+  FieldTypes = flux.FieldTypes, 
+  localStore = flux.backends.local,
+  local,
+  M = flux.createModel({
+    modelName: "model",
+    backend: localStore,
+    fieldTypes: {
+      field: FieldTypes.string
+    }
+  });
+
+local = {
+  reload: function(test) {
+    var m = new M({field: "asdf"});
+    m.save();
+    m.field = "qwer";
+    m.load();
+    test.strictEqual(m.field, "asdf");
+    test.done();
+  },
+  load: function (test) {
+    var m2, m1 = new M({field: "asdf"});
+    m1.save();
+    m1.field = "qwer";
+    m2 = M.load(m1.id);
+    test.strictEqual(m2.field, "asdf");
+    test.done();
+  }
+};
+
+module.exports = {
+  local: local,
+};
